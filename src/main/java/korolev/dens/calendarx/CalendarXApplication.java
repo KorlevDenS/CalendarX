a042caf7267bbcb8c60e7db102fe42d18efaaf57
Denis Korolev <333165@niuitmo.ru>
2026-09-18 20:04:29 +0300

    Created project Spring Boot 4.1.1 & maven

---
+++ src/main/java/korolev/dens/calendarx/CalendarXApplication.java
@@ -0,0 +1,13 @@
+package korolev.dens.calendarx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/korolev/dens/calendarx/CalendarXApplicationTests.java
@@ -0,0 +1,13 @@
+package korolev.dens.calendarx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
